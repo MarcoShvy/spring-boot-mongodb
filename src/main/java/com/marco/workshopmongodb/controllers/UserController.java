7b89795c3c18a5d652f7cc90cbc700aa9dfe7511
Marco <mass7813@gmail.com>
2024-03-22 23:53:33 -0300

    User DTO

--- src/main/java/com/marco/workshopmongodb/controllers/UserController.java
+++ src/main/java/com/marco/workshopmongodb/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.marco.workshopmongodb.controllers;
 
 
 import com.marco.workshopmongodb.domain.User;
+import com.marco.workshopmongodb.dto.UserDTO;
 import com.marco.workshopmongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -20,9 +22,10 @@ public class UserController {
     @Autowired
     private UserService userService;
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
 
         List<User> list = userService.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x.getId(), x.getName(), x.getEmail())).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 }
---
+++ src/main/java/com/marco/workshopmongodb/dto/UserDTO.java
@@ -0,0 +1,4 @@
+package com.marco.workshopmongodb.dto;
+
+public record UserDTO (String id, String name, String email){
+}
